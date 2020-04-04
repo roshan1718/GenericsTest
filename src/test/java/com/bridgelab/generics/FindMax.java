6a54871c20d5c676b9e1268df595f0ec23b9e689
roshan1718 <roshanshinde1717@gmail.com>
2020-04-04 15:22:43 +0530

    ADDED:UC 5 Method For Printing Maximum Result of Inputs

--- src/test/java/com/bridgelab/generics/FindMax.java
+++ src/test/java/com/bridgelab/generics/FindMax.java
@@ -2,25 +2,30 @@ package com.bridgelab.generics;
 
 import java.util.Arrays;
 
-public class FindMax <E extends Comparable<E>>{
+public class FindMax <E extends Comparable >{
 
     E[] arrayElements;
 
     //Default Constructor
     public FindMax(){
     }
-    public FindMax(E[] array) {
-        this.arrayElements=array;
+    public FindMax(E[] arrayElements) {
+        this.arrayElements=arrayElements;
     }
 
     //function for check maximum of given inputs
     public static <E extends Comparable> E checkMaximum(E... arrayElements )
     {
         Arrays.sort(arrayElements);
+        E maxResult=arrayElements[arrayElements.length-1];
+        printMax(maxResult);
         return arrayElements[arrayElements.length-1];
     }
-
-    {
-        System.out.println("Welcome To Find and Test Maximum Problem Using Generics");
+    // method for Printing Result
+    private static <E> void printMax(E maxResult) {
+        System.out.println("Largest Among all is..."+maxResult);
+    }
+    public static void main(String args[]) {
+        System.out.printf("Welcome To Test Maximum Problem");
     }
 }
--- src/test/java/com/bridgelab/generics/TestMax.java
+++ src/test/java/com/bridgelab/generics/TestMax.java
@@ -12,43 +12,43 @@ public class TestMax {
     }
     @Test
     public void givenThreeIntegers_FindMaxOfThem_ReturnMaximumTwo() {
-        Integer maxInteger = (Integer)maxObject.checkMaximum(15,22);
+        Integer maxInteger = maxObject.checkMaximum(15,22);
         Assert.assertEquals((Integer) 22, maxInteger);
     }
     @Test
     public void givenThreeIntegers_FindMaxOfThem_ReturnMaximumThree() {
-        Integer maxInteger = (Integer)maxObject.checkMaximum(5,12,33,54);
+        Integer maxInteger = maxObject.checkMaximum(5,12,33,54);
         Assert.assertEquals((Integer) 54, maxInteger);
     }
     @Test
     public void givenThreeFloatNumber_FindMaxOfThem_ReturnMaximumOne() {
-        Float maxFloat = (Float) maxObject.checkMaximum(55.36f,22.1f,3.54f,32.25f,27.35f);
+        Float maxFloat = maxObject.checkMaximum(55.36f,22.1f,3.54f,32.25f,27.35f);
         Assert.assertEquals((Float) 55.36f, maxFloat);
     }
 
     @Test
     public void givenThreeFloatNumber_FindMaxOfThem_ReturnMaximumTwo() {
-        Float maxFloat = (Float) maxObject.checkMaximum(15.36f,22.1f);
+        Float maxFloat =  maxObject.checkMaximum(15.36f,22.1f);
         Assert.assertEquals((Float) 22.1f, maxFloat);
     }
     @Test
     public void givenThreeFloatNumber_FindMaxOfThem_ReturnMaximumThree() {
-        Float maxFloat = (Float) maxObject.checkMaximum(5.36f,27.1f,32.5f,33.54f);
+        Float maxFloat = maxObject.checkMaximum(5.36f,27.1f,32.5f,33.54f);
         Assert.assertEquals((Float) 33.54f, maxFloat);
     }
     @Test
     public void givenThreeStrings_FindMaxOfThem_ReturnMaximumOne() {
-        String largestString = (String) maxObject.checkMaximum("Strawberry", "Apple", "Banana","Cherries");
+        String largestString = maxObject.checkMaximum("Strawberry", "Apple", "Banana","Cherries");
         Assert.assertEquals((String) "Strawberry", largestString);
     }
     @Test
     public void givenThreeStrings_FindMaxOfThem_ReturnMaximumTwo() {
-        String largestString = (String) maxObject.checkMaximum("Apple", "Strawberry");
+        String largestString = maxObject.checkMaximum("Apple", "Strawberry");
         Assert.assertEquals((String) "Strawberry", largestString);
     }
     @Test
     public void givenThreeStrings_FindMaxOfThem_ReturnMaximumThree() {
-        String largestString = (String) maxObject.checkMaximum("Apple","Mangoes", "Berries", "Strawberry");
+        String largestString =  maxObject.checkMaximum("Apple","Mangoes", "Berries", "Strawberry");
         Assert.assertEquals((String) "Strawberry", largestString);
     }
 }
