2d3599c18961c8ea19ada700126454f5237d8c85
roshan1718 <roshanshinde1717@gmail.com>
2020-04-04 13:59:12 +0530

    REFACTORED:1 Find Largest Of Three Inputs, All in one Method

--- src/test/java/com/bridgelab/generics/FindMax.java
+++ src/test/java/com/bridgelab/generics/FindMax.java
@@ -2,38 +2,15 @@ package com.bridgelab.generics;
 
 public class FindMax {
 
-    public static Integer findMaxInteger(Integer firstNumber, Integer secondNumber, Integer thirdNumber)
+    public static <E extends Comparable> E checkMaximum(E first, E second, E third)
     {
-        Integer maxInteger=firstNumber;
-        if (secondNumber.compareTo(maxInteger) > 0 )
-            maxInteger=secondNumber;
-        if ( thirdNumber.compareTo(maxInteger) > 0 )
-            maxInteger=thirdNumber;
-
-        return maxInteger;
+        E max=first;
+        if (second.compareTo(max) > 0 )
+            max=second;
+        if ( third.compareTo(max) > 0 )
+            max=third;
+        return max;
     }
 
-    public static Float findMaxFloatingNumber(Float firstNumber, Float secondNumber, Float thirdNumber)
-    {
-        Float maxFloatingNumber=firstNumber;
-        if (secondNumber.compareTo(maxFloatingNumber) > 0.0 )
-            maxFloatingNumber=secondNumber;
-        if ( thirdNumber.compareTo(maxFloatingNumber) > 0 )
-            maxFloatingNumber=thirdNumber;
-
-        return maxFloatingNumber;
-    }
-
-    public static String findLargestString(String firstString, String secondString, String thirdString)
-    {
-        String largestString=firstString;
-
-        if (secondString.compareTo(largestString) > 0 )
-            largestString=secondString;
-        if ( thirdString.compareTo(largestString) > 0 )
-            largestString=thirdString;
-
-        return largestString;
-    }
 
 }
--- src/test/java/com/bridgelab/generics/TestMax.java
+++ src/test/java/com/bridgelab/generics/TestMax.java
@@ -8,28 +8,28 @@ public class TestMax {
     @Test
     public void givenThreeIntegers_FindMaxOfThem_ReturnMaximumOne() {
      maxObject = new FindMax();
-        int integers = maxObject.findMaxInteger(5, 2, 3);
+        int integers = maxObject.checkMaximum(5, 2, 3);
         Assert.assertEquals(5, integers);
         System.out.println("Maximum Among All is.."+integers);
     }
     @Test
     public void givenThreeIntegers_FindMaxOfThem_ReturnMaximumTwo() {
         maxObject = new FindMax();
-        int integers = maxObject.findMaxInteger(12, 44, 23);
+        int integers = maxObject.checkMaximum(12, 44, 23);
         Assert.assertEquals(44, integers);
         System.out.println("Maximum Among All is.."+integers);
     }
     @Test
     public void givenThreeIntegers_FindMaxOfThem_ReturnMaximumThree() {
         maxObject = new FindMax();
-        int integers = maxObject.findMaxInteger(7, 15, 23);
+        int integers = maxObject.checkMaximum(7, 15, 23);
         Assert.assertEquals(23, integers);
         System.out.println("Maximum Among All is.."+integers);
     }
     @Test
     public void givenThreeFloatNumber_FindMaxOfThem_ReturnMaximumOne() {
         maxObject = new FindMax();
-        Float maxfloatNumber = FindMax.findMaxFloatingNumber(55.36f, 22.1f, 3.54f);
+        Float maxfloatNumber = FindMax.checkMaximum(55.36f, 22.1f, 3.54f);
         Assert.assertEquals((Float)55.36f, maxfloatNumber);
         System.out.println("Maximum Among All is.."+maxfloatNumber);
     }
@@ -37,32 +37,32 @@ public class TestMax {
     @Test
     public void givenThreeFloatNumber_FindMaxOfThem_ReturnMaximumTwo() {
         maxObject = new FindMax();
-        Float maxfloatNumber = FindMax.findMaxFloatingNumber(15.36f, 52.1f, 3.54f);
+        Float maxfloatNumber = FindMax.checkMaximum(15.36f, 52.1f, 3.54f);
         Assert.assertEquals((Float)52.1f, maxfloatNumber);
         System.out.println("Maximum Among All is.."+maxfloatNumber);
     }
     @Test
     public void givenThreeFloatNumber_FindMaxOfThem_ReturnMaximumThree() {
         maxObject = new FindMax();
-        Float maxfloatNumber = FindMax.findMaxFloatingNumber(15.36f, 12.1f, 43.54f);
+        Float maxfloatNumber = FindMax.checkMaximum(15.36f, 12.1f, 43.54f);
         Assert.assertEquals((Float)43.54f, maxfloatNumber);
         System.out.println("Maximum Among All is.."+maxfloatNumber);
     }
     @Test
     public void givenThreeStrings_FindMaxOfThem_ReturnMaximumOne() {
-        String largestString = FindMax.findLargestString("Strawberry", "Apple", "Aanana");
+        String largestString = FindMax.checkMaximum("Strawberry", "Apple", "Aanana");
         Assert.assertEquals("Strawberry", largestString);
         System.out.println("Largest String Among All is.."+largestString);
     }
     @Test
     public void givenThreeStrings_FindMaxOfThem_ReturnMaximumTwo() {
-        String largestString = FindMax.findLargestString("Apple", "Strawberry", "Banana");
+        String largestString = FindMax.checkMaximum("Apple", "Strawberry", "Banana");
         Assert.assertEquals("Strawberry", largestString);
         System.out.println("Largest String Among All is.."+largestString);
     }
     @Test
     public void givenThreeStrings_FindMaxOfThem_ReturnMaximumThree() {
-        String largestString = FindMax.findLargestString("Apple", "Berries", "Strawberry");
+        String largestString = FindMax.checkMaximum("Apple", "Berries", "Strawberry");
         Assert.assertEquals("Strawberry", largestString);
         System.out.println("Largest String Among All is.."+largestString);
     }
